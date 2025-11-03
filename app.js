3aaebd0583cf6c6cf3c0a6eeeaaddb58da020391
Alkaf Khan <@khanalkaf308@gmail.com>
2025-11-03 23:38:07 +0530

    Added Both Clent and Server Side Validation and implimented error handling

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate")
+const wrapAsync = require("./utils/wrapAsync.js")
+const ExpressError = require("./utils/ExpressError.js")
+const { ListingSchema} = require("./schema.js")
 
 
 //seting view engine as ejs
@@ -37,56 +40,83 @@ app.get("/",(req,res)=>{
 })
 
 
+//validating listing using Joi (with the of a validate listing middleware function)
+const validateListing = (req, res , next )=>{
+    let {error} = ListingSchema.validate(req.body);
+    if(error){
+        let errMsg = error.details.map((el)=> el.message).join();
+        throw new ExpressError(400,errMsg);
+    }else{
+        next();
+    }
+}
+
 //index route or listing route
-app.get("/listings",async (req,res)=>{
+app.get("/listings",wrapAsync( async (req,res)=>{
     const allListings = await Listing.find();
     res.render("listings/index.ejs", {allListings})
-})
+}))
 //new route
 app.get("/listings/new",(req,res)=>{
     res.render("listings/new.ejs")
 });
 //create route
-app.post("/listings",async (req,res)=>{
-    const newListing = new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");
-})
+app.post("/listings",validateListing, wrapAsync( async (req,res,next)=>{
+        // if(!req.body.listing){
+        //     throw new ExpressError(400 , "send valid data for listing")
+        // } //insted
+        
+        const newListing = new Listing(req.body.listing);
+        await newListing.save();
+        res.redirect("/listings");
+}))
 
 //edit route
-app.get("/listings/:id/edit",async(req,res)=>{
+app.get("/listings/:id/edit",wrapAsync( async(req,res)=>{
     let {id} = req.params;
     let listing = await Listing.findById(id);
     res.render("listings/edit.ejs",{listing})
-});
+}));
 
 //update route
-app.put("/listings/:id",async (req,res)=>{
+app.put("/listings/:id" ,validateListing,wrapAsync( async (req,res)=>{
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id , {...req.body.listing});
     res.redirect(`/listings/${id}`)
-});
+}));
 
 //delete route
-app.delete("/listings/:id",async (req,res)=>{
+app.delete("/listings/:id",wrapAsync( async (req,res)=>{
     let {id} = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     // console.log("DELETED LISTING",deletedListing)
     res.redirect(`/listings`)
-});
+}));
 
 
 //read or show route
-app.get("/listings/:id",async(req,res)=>{
+app.get("/listings/:id",wrapAsync( async(req,res)=>{
     let {id} = req.params;
     let listing = await Listing.findById(id);
     // console.log(listing)
     res.render("listings/show.ejs",{listing})
 
+}))
+
+//for req which does not match any route handler
+app.use((req, res, next) => {
+    next(new ExpressError(404, "Page not found"));
+});
+
+
+
+
+//handling error using middleware
+app.use((err,req,res,next)=>{
+    let { statusCode= 500 , message= "something went wrong"} = err;
+    // res.status(statusCode).send(message);
+    res.status(statusCode).render("error.ejs" ,{message})
 })
-
-
-
 
 app.listen(port ,(req,res)=>{
     console.log('listning on port : ', port);
